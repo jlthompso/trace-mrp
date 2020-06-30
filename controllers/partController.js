a4e6d013a8729598ff935bf858a35e7d69a6bd51
Joe Thompson <jthompso@mtu.edu>
2020-06-29 21:26:54 -0700

    Added create part form

--- controllers/partController.js
+++ controllers/partController.js
@@ -1,8 +1,14 @@
 const Part = require('../models/part')
 
 // Display list of all Parts
-exports.part_list = function(req, res) {
-    res.render('inventory', {title: 'traceMRP', path: `/inventory${req.path}`})
+exports.part_list = function(req, res, next) {
+    Part.find({}, 'title author')
+        .populate('author')
+        .exec(function (err, list_parts) {
+            if (err) {return next(err)}
+            //Successful, so render
+            res.render('part_list', {title: 'traceMRP', part_list: list_parts, path: `/inventory${req.path}`})
+    }) 
 }
 
 // Display detail page for a specific Part
@@ -11,8 +17,8 @@ exports.part_detail = function(req, res) {
 }
 
 // Display Part create form on GET
-exports.part_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Part create GET')
+exports.part_create_get = function(req, res, next) {
+    res.render('part_form', {title: 'traceMRP', path: `/inventory${req.path}`})
 }
 
 // Handle Part create on POST
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'traceMRP' });
-});
+  res.render('index', {title: 'traceMRP', path: `/${res.path}`})
+})
 
-module.exports = router;
+module.exports = router

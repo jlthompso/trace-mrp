cedc7da6115c73cb7a98742ee89b60250ff9657f
Joe Thompson <jthompso@mtu.edu>
2020-07-03 11:38:19 -0700

    Add part to database

--- controllers/partController.js
+++ controllers/partController.js
@@ -2,10 +2,9 @@ const Part = require('../models/part')
 
 // Display list of all Parts
 exports.part_list = function(req, res, next) {
-    Part.find({}, 'title author')
-        .populate('author')
+    Part.find()
         .exec(function (err, list_parts) {
-            if (err) {return next(err)}
+            if (err) return next(err)
             //Successful, so render
             res.render('part_list', {title: 'traceMRP', part_list: list_parts, path: `/inventory${req.path}`})
     }) 
@@ -22,8 +21,30 @@ exports.part_create_get = function(req, res, next) {
 }
 
 // Handle Part create on POST
-exports.part_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Part create POST')
+exports.part_create_post = function(req, res, next) {
+    console.log(req.body)
+    let part = new Part({
+        number: req.body.partNumber,
+        name: req.body.partName,
+        unitOfMeasure: req.body.unitOfMeasure,
+        revision: req.body.partRev
+    })
+
+    Part.findOne({'number': req.body.number})
+        .exec(function(err, found_part) {
+            if (err) return next(err)
+
+            if (found_part) {
+                // Part exists, redirect to its detail page
+                res.redirect(found_part.url)
+            } else {
+                part.save(function (err) {
+                    if (err) return next(err)
+                    // Part saved, redirect to its detail page
+                    res.redirect(part.url)
+                })
+            }
+        })
 }
 
 // Display Part delete form on GET
